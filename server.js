6f02210cf787d7263946368d7591ea3e46ef34f6
Howard Yang <howardyang25@gmail.com>
2019-09-26 19:41:56 -0700

    All 4 components are rendered successfully

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(cors());
 // app.use(express.static(path.resolve(__dirname, './public')));
 app.use(`/listing/:id`, express.static(path.resolve(__dirname, 'public')));
 
+app.get('/api/listing/:listingID/photos', (req, res) => {
+  const { listingID } = req.params;
+  res.redirect(`http://ec2-54-183-105-172.us-west-1.compute.amazonaws.com:3100/api/listing/${listingID}`);
+ });
+
+
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
